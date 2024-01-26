83f6c09c552cee2e93e893625b684e96a270d2d5
20ml <milliliter.avc@gmail.com>
2024-01-26 13:02:52 -0500

    Add argument typing helpers for editor

--- lib/cardbehavior/CardBehaviorArgument.gd
+++ lib/cardbehavior/CardBehaviorArgument.gd
@@ -7,6 +7,20 @@ var name : StringName
 func _init() -> void:
 	push_error("CardBehaviorArgument is not to be instanced manually. Use the static functions to create new instances.")
 
+func get_type_as_int() -> int:
+	return CardBehaviorArgument.type_to_int(self.type)
+
+# enum??
+static func type_to_int(_type : StringName) -> int:
+	return [
+		"int", 
+		"float",
+		"bool",
+		"StringName",
+		"Area2D",
+		"TargetableComponent",
+	].find(_type)
+
 static func int(_name : StringName) -> CardBehaviorArgument:
 	var argument : CardBehaviorArgument = CardBehaviorArgument.new()
 	argument.type = "int"
