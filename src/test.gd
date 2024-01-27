55bc0134e4035c82cc386fe54919feec6084903c
20ml <milliliter.avc@gmail.com>
2024-01-26 20:55:02 -0500

    Do a GDScript test

--- src/test.gd
+++ src/test.gd
@@ -1,14 +1,10 @@
 class_name Test
 extends Node
 
-@export var meta : CardMetadata = null
-@onready var hui : HandUI = get_parent()
-
-var did : bool = false
-func _process(_delta : float) -> void:
-	if did: return
-	hui._handle_hand_update({
-		"type" : "add",
-		"metadata" : meta
-	})
-	did = true
+func _ready() -> void:
+	var arr : Array[int] = [2]
+	for num in arr:
+		print(num)
+		arr.append(num * 2)
+		if num > 100:
+			return
