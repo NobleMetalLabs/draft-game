f3714a24c7abac899853a72b9bb3517f359980bc
20ml <milliliter.avc@gmail.com>
2024-01-26 19:56:34 -0500

    Rework processing strategy, create UML

--- lib/cardbehavior/CardBehaviorNodeInstance.gd
+++ lib/cardbehavior/CardBehaviorNodeInstance.gd
@@ -5,11 +5,5 @@ var node_logic : CardBehaviorNode
 var inputs : Array[CardBehaviorNodeInstance]
 var outputs : Array[CardBehaviorNodeInstance]
 
-var cached_output : Array
-
-func fetch() -> Array:
-	return cached_output
-
 func process(input : Array) -> Array:
-	cached_output = node_logic.process(input)
-	return cached_output
+	return node_logic.process(input)
